cbabba55c77e986b97c5e93dee94b16330a58364
Jeremy Roy <46678222+Mellowlink@users.noreply.github.com>
2021-12-18 00:59:28 +0900

    Remove unused imports

--- coredrill_trader.py
+++ coredrill_trader.py
@@ -1,11 +1,6 @@
 import kivy
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.togglebutton import ToggleButton
 from kivy.properties import ObjectProperty
 
 class DashboardLayout(Widget):
