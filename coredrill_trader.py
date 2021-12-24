d1b57f85869b807be32dd5de0e39ed74424eb0c4
Jeremy Roy <46678222+Mellowlink@users.noreply.github.com>
2021-12-25 00:42:46 +0900

    Test code to grab exchange position data

--- coredrill_trader.py
+++ coredrill_trader.py
@@ -69,8 +69,11 @@ class DashboardLayout(Widget):
             instance.active = False
             print(f'Initialise credentials here: {instance.active}')
         elif has_credentials and instance.active:
+            #try init exchange here
             print(f'OK to connect: {instance.active}')
             self.toggle_interface(instance.active)
+
+            #exception handling here
         else:
             print(f'Connection off: {instance.active}')
             self.toggle_interface(instance.active)
@@ -95,6 +98,44 @@ class DashboardLayout(Widget):
 
 
 class CoreDrill(MDApp):
+
+    async def fetch_position(self) -> dict:
+        positions, account, funding = await asyncio.gather(
+            self.exchange.fapiPrivate_get_positionrisk(params={'symbol': 'ETHUSDT'}),
+            self.exchange.fapiPrivate_get_account(),
+            self.exchange.fapiPublic_get_fundingrate()
+        )
+        if positions:
+            position = {'size': float(positions[0]['positionAmt']),
+                        'price': float(positions[0]['entryPrice']),
+                        'liquidation_price': float(positions[0]['liquidationPrice']),
+                        'leverage': float(positions[0]['leverage'])}
+        else:
+            position = {'size': 0.0,
+                        'price': 0.0,
+                        'liquidation_price': 0.0,
+                        'leverage': 1.0}
+        for e in funding:
+            if e['symbol'] == 'ETHUSDT':
+                position['funding_time'] = float(e['fundingTime'])
+                position['predicted_funding_rate'] = float(e['fundingRate'])
+                break
+        for e in account['assets']:
+            if e['asset'] == 'USDT':
+                position['margin_cost'] = float(e['positionInitialMargin'])
+                position['equity'] = float(e['marginBalance'])
+                position['wallet_balance'] = float(e['walletBalance'])
+                position['available_balance'] = float(e['availableBalance'])
+                break
+        print('Final position: ') #test
+        print(position)
+
+    def init_ccxt(self):
+        self.exchange = getattr(ccxt_async, 'binance')({'apiKey': key,
+                                            'secret': secret,
+                                            'options': {'defaultType': 'future'}})
+        asyncio.run(self.fetch_position())
+
     def build(self):
         Config.set('input', 'mouse', 'mouse,disable_multitouch')
         Window.size = (1024, 600)
@@ -102,6 +143,8 @@ class CoreDrill(MDApp):
         if not os.path.exists(config_path):
             os.makedirs(config_path)
             print("Created configuration folder.")
+
+        self.init_ccxt()
 
         return DashboardLayout()
 
